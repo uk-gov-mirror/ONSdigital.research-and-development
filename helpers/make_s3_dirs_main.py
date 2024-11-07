7745fe37e981f23834506a8e523b9b33d70bd53f
Anne Griffith <Anne.Griffith@ons.gov.uk>
2024-11-07 15:40:15 +0000

    add s3 settings in mini config

--- helpers/make_s3_dirs_main.py
+++ helpers/make_s3_dirs_main.py
@@ -1,7 +1,6 @@
 """Script that creates all directories"""
 import os
 import sys
-sys.path.append("D:/research-and-development")
 
 # Change to the project repository location
 my_wd = os.getcwd()
@@ -13,7 +12,14 @@ from src.utils.helpers import tree_to_list
 import src.utils.s3_mods as mods
 from src.utils.singleton_boto import SingletonBoto
 
-boto3_client = SingletonBoto.get_client()
+config = {
+    "s3": {
+        "ssl_file": "/etc/pki/tls/certs/ca-bundle.crt",
+        "s3_bucket": "onscdp-dev-data01-5320d6ca"
+    }
+}
+
+boto3_client = SingletonBoto.get_client(config)
 
 
 def run_make_dirs():
@@ -83,7 +89,62 @@ def run_make_dirs():
                 "v1": {},
             },
             "PNP": {
-                "01_staging": {},
+                "01_staging": {
+                    "feather": {},
+                    "staging_qa": {
+                        "full_responses_qa": {},
+                        "postcode_validation": {},
+                        "Postcodes for QA": {},
+                    },
+                },
+                "02_freezing": {
+                    "changes_to_review": {},
+                    "freezing_updates": {},
+                    "frozen_data_staged": {},
+                },
+                "03_northern_ireland": {
+                    "2021": {},
+                    "ni_staging_qa": {},
+                },
+                "04_construction": {
+                    "manual_construction": {},
+                },
+                "05_mapping": {
+                    "mapping_qa": {},
+                },
+                "06_imputation": {
+                    "backdata_output": {},
+                    "imputation_qa": {},
+                    "manual_trimming": {},
+                },
+                "07_outliers": {
+                    "auto_outliers": {},
+                    "manual_outliers": {},
+                    "outliers_qa": {},
+                },
+                "08_estimation": {
+                    "estimation_qa": {},
+                },
+                "09_apportionment": {
+                    "apportionment_qa": {},
+                },
+                "10_outputs": {
+                    "output_fte_total_qa": {},
+                    "output_gb_sas": {},
+                    "output_intram_by_civil_defence": {},
+                    "output_intram_by_pg_gb": {},
+                    "output_intram_by_pg_uk": {},
+                    "output_intram_by_sic": {},
+                    "output_intram_gb_itl1": {},
+                    "output_intram_gb_itl2": {},
+                    "output_intram_uk_itl1": {},
+                    "output_intram_uk_itl2": {},
+                    "output_long_form": {},
+                    "output_ni_sas": {},
+                    "output_short_form": {},
+                    "output_status_filtered_qa": {},
+                    "output_tau": {},
+                },
             },
         },
     }
