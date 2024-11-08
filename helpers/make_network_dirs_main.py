fd69407314549a413ad1dae00341ff866be280c1
Lewis <ryan.lewis@ons.gov.uk>
2024-11-08 14:55:26 +0000

    updated scripts to address PR comment

--- helpers/make_network_dirs_main.py
+++ helpers/make_network_dirs_main.py
@@ -1,9 +1,9 @@
 """Script that creates all directories"""
+import os
 import sys
 sys.path.append("D:/research-and-development")
 from src.utils.helpers import tree_to_list
 from src.utils.local_file_mods import rd_file_exists
-import os
 
 # Change to the project repository location
 my_wd = os.getcwd()
@@ -27,144 +27,89 @@ def run_make_dirs():
     """Creates all directories based on the `tree` structure.
 
     tree (dict) converted to list of lists by function src.utils.helpers.tree_to_list.
-    make_dir function is called for item in the list of directories.
+    make_dir function is called for item in the list of directories. Itertates through
+    years (list) and surveys (list) to create the directories for said years and
+    surveys.
 
     """
 
     root = "BERD Results System Development 2023/DAP_emulation"
 
-    tree = {
-        "2022_surveys": {
-            "PNP": {
-                "01_staging": {
-                    "feather": {},
-                    "staging_qa": {
-                        "full_responses_qa": {},
-                        "postcode_validation": {},
-                        "Postcodes for QA": {},
-                    },
-                },
-                "02_freezing": {
-                    "changes_to_review": {},
-                    "freezing_updates": {},
-                    "frozen_data_staged": {},
-                },
-                "03_northern_ireland": {
-                    "2022": {},
-                    "ni_staging_qa": {},
-                },
-                "04_construction": {
-                    "manual_construction": {},
-                },
-                "05_mapping": {
-                    "mapping_qa": {},
-                },
-                "06_imputation": {
-                    "backdata_output": {},
-                    "imputation_qa": {},
-                    "manual_trimming": {},
-                },
-                "07_outliers": {
-                    "auto_outliers": {},
-                    "manual_outliers": {},
-                    "outliers_qa": {},
-                },
-                "08_estimation": {
-                    "estimation_qa": {},
-                },
-                "09_apportionment": {
-                    "apportionment_qa": {},
-                },
-                "10_outputs": {
-                    "output_fte_total_qa": {},
-                    "output_gb_sas": {},
-                    "output_intram_by_civil_defence": {},
-                    "output_intram_by_pg_gb": {},
-                    "output_intram_by_pg_uk": {},
-                    "output_intram_by_sic": {},
-                    "output_intram_gb_itl1": {},
-                    "output_intram_gb_itl2": {},
-                    "output_intram_uk_itl1": {},
-                    "output_intram_uk_itl2": {},
-                    "output_long_form": {},
-                    "output_ni_sas": {},
-                    "output_short_form": {},
-                    "output_status_filtered_qa": {},
-                    "output_tau": {},
-                },
-            },
-            "mappers": {
-                "v1": {},
-            }
-        },
-        "2023_surveys": {
-            "PNP": {
-                "01_staging": {
-                    "feather": {},
-                    "staging_qa": {
-                        "full_responses_qa": {},
-                        "postcode_validation": {},
-                        "Postcodes for QA": {},
-                    },
-                },
-                "02_freezing": {
-                    "changes_to_review": {},
-                    "freezing_updates": {},
-                    "frozen_data_staged": {},
-                },
-                "03_northern_ireland": {
-                    "2023": {},
-                    "ni_staging_qa": {},
-                },
-                "04_construction": {
-                    "manual_construction": {},
-                },
-                "05_mapping": {
-                    "mapping_qa": {},
-                },
-                "06_imputation": {
-                    "backdata_output": {},
-                    "imputation_qa": {},
-                    "manual_trimming": {},
-                },
-                "07_outliers": {
-                    "auto_outliers": {},
-                    "manual_outliers": {},
-                    "outliers_qa": {},
-                },
-                "08_estimation": {
-                    "estimation_qa": {},
-                },
-                "09_apportionment": {
-                    "apportionment_qa": {},
-                },
-                "10_outputs": {
-                    "output_fte_total_qa": {},
-                    "output_gb_sas": {},
-                    "output_intram_by_civil_defence": {},
-                    "output_intram_by_pg_gb": {},
-                    "output_intram_by_pg_uk": {},
-                    "output_intram_by_sic": {},
-                    "output_intram_gb_itl1": {},
-                    "output_intram_gb_itl2": {},
-                    "output_intram_uk_itl1": {},
-                    "output_intram_uk_itl2": {},
-                    "output_long_form": {},
-                    "output_ni_sas": {},
-                    "output_short_form": {},
-                    "output_status_filtered_qa": {},
-                    "output_tau": {},
-                },
-            },
-            "mappers": {
-                "v1": {},
-            }
-        },
-    }
+    years = ["2022", "2023"]
+    surveys = ["BERD", "PNP",]
 
-    dir_list = tree_to_list(tree, prefix=root, path_list=[])
-    for s in dir_list:
-        make_dir(s)
+    for year in years:
+        for survey in surveys:
+
+            tree = {
+                f"{year}_surveys": {
+                    f"{survey}": {
+                        "01_staging": {
+                            "feather": {},
+                            "staging_qa": {
+                                "full_responses_qa": {},
+                                "postcode_validation": {},
+                                "Postcodes for QA": {},
+                            },
+                        },
+                        "02_freezing": {
+                            "changes_to_review": {},
+                            "freezing_updates": {},
+                            "frozen_data_staged": {},
+                        },
+                        "03_northern_ireland": {
+                            f"{year}": {},
+                            "ni_staging_qa": {},
+                        },
+                        "04_construction": {
+                            "manual_construction": {},
+                        },
+                        "05_mapping": {
+                            "mapping_qa": {},
+                        },
+                        "06_imputation": {
+                            "backdata_output": {},
+                            "imputation_qa": {},
+                            "manual_trimming": {},
+                        },
+                        "07_outliers": {
+                            "auto_outliers": {},
+                            "manual_outliers": {},
+                            "outliers_qa": {},
+                        },
+                        "08_estimation": {
+                            "estimation_qa": {},
+                        },
+                        "09_apportionment": {
+                            "apportionment_qa": {},
+                        },
+                        "10_outputs": {
+                            "output_fte_total_qa": {},
+                            "output_gb_sas": {},
+                            "output_intram_by_civil_defence": {},
+                            "output_intram_by_pg_gb": {},
+                            "output_intram_by_pg_uk": {},
+                            "output_intram_by_sic": {},
+                            "output_intram_gb_itl1": {},
+                            "output_intram_gb_itl2": {},
+                            "output_intram_uk_itl1": {},
+                            "output_intram_uk_itl2": {},
+                            "output_long_form": {},
+                            "output_ni_sas": {},
+                            "output_short_form": {},
+                            "output_status_filtered_qa": {},
+                            "output_tau": {},
+                        },
+                    },
+                    "mappers": {
+                        "v1": {},
+                    }
+                },
+            }
+
+            dir_list = tree_to_list(tree, prefix=root, path_list=[])
+            for s in dir_list:
+                make_dir(s)
 
 
 if __name__ == "__main__":
--- helpers/make_s3_dirs_main.py
+++ helpers/make_s3_dirs_main.py
@@ -5,8 +5,9 @@ import sys
 # src is in the parent directory so append this to path
 sys.path.append('..')
 
-from src.utils.singleton_boto import SingletonBoto
 from src.utils.helpers import tree_to_list
+from src.utils.singleton_boto import SingletonBoto
+import src.utils.s3_mods as mods
 
 config = {
     "s3": {
@@ -21,134 +22,83 @@ import src.utils.s3_mods as mods
 def run_make_dirs():
     root = "/bat/res_dev/project_data"
 
-    tree = {
-        "2021_surveys": {
-            "BERD": {
-                "01_staging": {
-                    "feather": {},
-                    "staging_qa": {
-                        "full_responses_qa": {},
-                        "postcode_validation": {},
-                        "Postcodes for QA": {},
+    years = ["2022", "2023"]
+    surveys = ["BERD", "PNP",]
+
+    for year in years:
+        for survey in surveys:
+
+            tree = {
+                f"{year}_surveys": {
+                    f"{survey}": {
+                        "01_staging": {
+                            "feather": {},
+                            "staging_qa": {
+                                "full_responses_qa": {},
+                                "postcode_validation": {},
+                                "Postcodes for QA": {},
+                            },
+                        },
+                        "02_freezing": {
+                            "changes_to_review": {},
+                            "freezing_updates": {},
+                            "frozen_data_staged": {},
+                        },
+                        "03_northern_ireland": {
+                            f"{year}": {},
+                            "ni_staging_qa": {},
+                        },
+                        "04_construction": {
+                            "manual_construction": {},
+                        },
+                        "05_mapping": {
+                            "mapping_qa": {},
+                        },
+                        "06_imputation": {
+                            "backdata_output": {},
+                            "imputation_qa": {},
+                            "manual_trimming": {},
+                        },
+                        "07_outliers": {
+                            "auto_outliers": {},
+                            "manual_outliers": {},
+                            "outliers_qa": {},
+                        },
+                        "08_estimation": {
+                            "estimation_qa": {},
+                        },
+                        "09_apportionment": {
+                            "apportionment_qa": {},
+                        },
+                        "10_outputs": {
+                            "output_fte_total_qa": {},
+                            "output_gb_sas": {},
+                            "output_intram_by_civil_defence": {},
+                            "output_intram_by_pg_gb": {},
+                            "output_intram_by_pg_uk": {},
+                            "output_intram_by_sic": {},
+                            "output_intram_gb_itl1": {},
+                            "output_intram_gb_itl2": {},
+                            "output_intram_uk_itl1": {},
+                            "output_intram_uk_itl2": {},
+                            "output_long_form": {},
+                            "output_ni_sas": {},
+                            "output_short_form": {},
+                            "output_status_filtered_qa": {},
+                            "output_tau": {},
+                        },
                     },
+                    "mappers": {
+                        "v1": {},
+                    }
                 },
-                "02_freezing": {
-                    "changes_to_review": {},
-                    "freezing_updates": {},
-                    "frozen_data_staged": {},
-                },
-                "03_northern_ireland": {
-                    "2021": {},
-                    "ni_staging_qa": {},
-                },
-                "04_construction": {
-                    "manual_construction": {},
-                },
-                "05_mapping": {
-                    "mapping_qa": {},
-                },
-                "06_imputation": {
-                    "backdata_output": {},
-                    "imputation_qa": {},
-                    "manual_trimming": {},
-                },
-                "07_outliers": {
-                    "auto_outliers": {},
-                    "manual_outliers": {},
-                    "outliers_qa": {},
-                },
-                "08_estimation": {
-                    "estimation_qa": {},
-                },
-                "09_apportionment": {
-                    "apportionment_qa": {},
-                },
-                "10_outputs": {
-                    "output_fte_total_qa": {},
-                    "output_gb_sas": {},
-                    "output_intram_by_civil_defence": {},
-                    "output_intram_by_pg_gb": {},
-                    "output_intram_by_pg_uk": {},
-                    "output_intram_by_sic": {},
-                    "output_intram_gb_itl1": {},
-                    "output_intram_gb_itl2": {},
-                    "output_intram_uk_itl1": {},
-                    "output_intram_uk_itl2": {},
-                    "output_long_form": {},
-                    "output_ni_sas": {},
-                    "output_short_form": {},
-                    "output_status_filtered_qa": {},
-                    "output_tau": {},
-                },
-            },
-            "mappers": {
-                "v1": {},
-            },
-            "PNP": {
-                "01_staging": {
-                    "feather": {},
-                    "staging_qa": {
-                        "full_responses_qa": {},
-                        "postcode_validation": {},
-                        "Postcodes for QA": {},
-                    },
-                },
-                "02_freezing": {
-                    "changes_to_review": {},
-                    "freezing_updates": {},
-                    "frozen_data_staged": {},
-                },
-                "03_northern_ireland": {
-                    "2021": {},
-                    "ni_staging_qa": {},
-                },
-                "04_construction": {
-                    "manual_construction": {},
-                },
-                "05_mapping": {
-                    "mapping_qa": {},
-                },
-                "06_imputation": {
-                    "backdata_output": {},
-                    "imputation_qa": {},
-                    "manual_trimming": {},
-                },
-                "07_outliers": {
-                    "auto_outliers": {},
-                    "manual_outliers": {},
-                    "outliers_qa": {},
-                },
-                "08_estimation": {
-                    "estimation_qa": {},
-                },
-                "09_apportionment": {
-                    "apportionment_qa": {},
-                },
-                "10_outputs": {
-                    "output_fte_total_qa": {},
-                    "output_gb_sas": {},
-                    "output_intram_by_civil_defence": {},
-                    "output_intram_by_pg_gb": {},
-                    "output_intram_by_pg_uk": {},
-                    "output_intram_by_sic": {},
-                    "output_intram_gb_itl1": {},
-                    "output_intram_gb_itl2": {},
-                    "output_intram_uk_itl1": {},
-                    "output_intram_uk_itl2": {},
-                    "output_long_form": {},
-                    "output_ni_sas": {},
-                    "output_short_form": {},
-                    "output_status_filtered_qa": {},
-                    "output_tau": {},
-                },
-            },
-        },
-    }
-    dir_list = tree_to_list(tree, prefix=root, path_list=[])
-    for s in dir_list:
-        print(s)
-        mods.rd_mkdir(s)
-    print(f"Created {len(dir_list)} directory(ies) succesfully.")
+            }
+
+            dir_list = tree_to_list(tree, prefix=root, path_list=[])
+            for s in dir_list:
+                print(s)
+                mods.rd_mkdir(s)
+            print(f"Created {len(dir_list)} directory(ies) succesfully.")
 
 
 if __name__ == "__main__":
