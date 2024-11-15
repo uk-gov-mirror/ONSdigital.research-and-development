6a92fa6afca223fd723f6558c330233bf3e8ec95
woodac <Clarissa.Woodage@ons.gov.uk>
2024-11-15 12:33:22 +0000

    Short-form test now passes

--- tests/test_outputs/test_short_form.py
+++ tests/test_outputs/test_short_form.py
@@ -30,12 +30,12 @@ class TestCreateHeadcountCols:
             "reference", "701", "702", "703", "704", "705", "706", "707", "709", "710", "711", "formtype", "status", "headcount_civil", "headcount_defence"
         ]
         data = [
-            [1, np.nan, np.nan, np.nan, np.nan, 100.0, 20.0, 10.0, np.nan, np.nan, np.nan, "0006", "Clear", 66.6667, 33.3333],
-            [2, np.nan, np.nan, np.nan, np.nan, 0.0, 0.0, 0.0, np.nan, np.nan, np.nan, "0006", "Clear", 0.0, 0.0],
-            [3, np.nan, np.nan, np.nan, np.nan, 200.0, 80.0, 0.0, np.nan, np.nan, np.nan, "0006", "Clear", 200.0, 0.0],
-            [4, np.nan, np.nan, np.nan, np.nan, 300.0, 0.0, 0.0, np.nan, np.nan, np.nan, "0006", "Clear", 0.0, 0.0],
-            [5, np.nan, np.nan, np.nan, np.nan, np.nan, 10.0, np.nan, np.nan, np.nan, np.nan, "0006", "Clear", 0.0, 0.0],
-            [6, np.nan, np.nan, np.nan, np.nan, 400, 20, np.nan, np.nan, np.nan, np.nan, "0006", "Clear", 400, 0]
+            [1, 0.0, 0.0, 0.0, 0.0, 100.0, 20.0, 10.0, 0.0, 0.0, 0.0, "0006", "Clear", 66.6667, 33.3333],
+            [2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, "0006", "Clear", 0.0, 0.0],
+            [3, 0.0, 0.0, 0.0, 0.0, 200.0, 80.0, 0.0, 0.0, 0.0, 0.0, "0006", "Clear", 200.0, 0.0],
+            [4, 0.0, 0.0, 0.0, 0.0, 300.0, 0.0, 0.0, 0.0, 0.0, 0.0, "0006", "Clear", 0.0, 0.0],
+            [5, 0.0, 0.0, 0.0, 0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, "0006", "Clear", 0.0, 0.0],
+            [6, 0.0, 0.0, 0.0, 0.0, 400, 20, 0.0, 0.0, 0.0, 0.0, "0006", "Clear", 400, 0]
         ]
         expected_data_df = pd.DataFrame(data=data, columns=columns)
         return expected_data_df
