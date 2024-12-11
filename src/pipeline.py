0e19e608109f42215b8c3e5ee09ee4795d4cfb2a
Ryan Lewis <40635968+Ryan2Y79@users.noreply.github.com>
2024-12-11 10:51:02 +0000

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -169,12 +169,11 @@ def run_pipeline(user_config_path, dev_config_path):
 
     # Mapping module
     MainLogger.info("Starting Mapping...")
-    (mapped_df, ni_full_responses, itl_mapper, backdata) = run_mapping(
+    (mapped_df, ni_full_responses, itl_mapper) = run_mapping(
         full_responses,
         ni_df,
         postcode_mapper,
         config,
-        backdata,
         mods.rd_read_csv,
         mods.rd_write_csv,
         mods.rd_file_exists,
