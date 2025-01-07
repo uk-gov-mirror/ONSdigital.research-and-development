fc9d32c3b13c0327e986ec35558df35bdfe5ef79
Anne Griffith <anne.griffith@ons.gov.uk>
2025-01-07 09:57:39 +0000

    reorder empty cols for ease of reading

--- helpers/create_pnp_backdata.py
+++ helpers/create_pnp_backdata.py
@@ -300,7 +300,7 @@ def add_missing_columns(df):
     Return:
         df (pd.DataFrame): The dataframe with the added missing columns.
     """
-    missing_list = ['226', '228', '237', '203', '225', '227', '229', '251',
+    missing_list = ['203', '225', '226', '227', '228', '229', '237', '251',
                     '300', '301', '307', '308', '309', '708',
                     'survey', 'formid', 'cellnumber', 'pg_numeric']
     for col in missing_list:
