25d156287a003278cbdd78be40e8ff5a38141d29
woodac <Clarissa.Woodage@ons.gov.uk>
2025-01-14 11:24:40 +0000

    refactoring script for perfomance

--- helpers/regression_imputation.py
+++ helpers/regression_imputation.py
@@ -11,16 +11,6 @@ Saves the outputs
 #%% Configuration settings
 import pandas as pd
 
-def write_csv(file_path: str, df: pd.DataFrame) -> None:
-    """Write a DataFrame to a CSV file.
-
-    Args:
-        file_path (str): The path to the output CSV file.
-        df (pd.DataFrame): The DataFrame to write to the CSV file.
-    """
-    df.to_csv(file_path, index=False)
-
-
 # Input folder and file names
 root_path = "R:/BERD Results System Development 2023/DAP_emulation/2023_surveys/BERD/06_imputation/imputation_qa/"
 in_file_old = "2023_full_responses_imputed_25-01-14_v318.csv"
@@ -42,44 +32,41 @@ other_cols = [
     "status",
 ]
 tolerance = 0.001
-#%% Read files
+# %% Read files
 cols_read = key_cols + [value_col] + other_cols
 df_old = pd.read_csv((root_path + in_file_old), low_memory=False)
 df_new = pd.read_csv((root_path + in_file_new), low_memory=False)
 
-#%% join old and new
+# %% join old and new
 df_merge = df_old.merge(df_new, on=key_cols, how="inner", suffixes=("_old", "_new"))
 
-#%%
+# %%
 df_merge.to_csv(root_path + out_file, index=False)
 
-
-#%% Filter good statuses only
+# %% Filter good statuses only
 imp_markers_to_keep = ["TMI", "CF", "MoR", "constructed"]
 df_old_good = df_old[df_old["imp_marker"].isin(imp_markers_to_keep)]
 df_new_good = df_new[df_new["imp_marker"].isin(imp_markers_to_keep)]
 
-#%% sizes
+# %% sizes
 print(f"Old size: {df_old_good.shape}")
 print(f"New size: {df_new_good.shape}")
 
-#%% Creating copy of df to improve fragmentation and performance
+# %%  Creating copy of df to produce a defragmented df and improve performance
 df_merge = df_merge.copy()
 
-#%% Join
+# %% Join
 df_merge = df_old_good.merge(
     df_new_good, on=key_cols, how="outer", suffixes=("_old", "_new"), indicator=True
 )
 
-#%% Creating copy of df to improve fragmentation and performance
+# %% Creating copy of df to produce a defragmented df and improve performance
 df_new = df_merge.copy()
 
-#%% Compare the values
+# %% Compare the values
 df_new["value_different"] = (
     df_new[value_col + "_old"] - df_new[value_col + "_new"]
 ) ** 2 > tolerance**2
 
 # %% Save output
-write_csv(out_fol + out_file, df_new)
-
-# %%
+df_new.to_csv(out_fol + out_file, index=False)
