8bd3e2cc4ab42e519714bd8fd871746a996acfd1
woodac <Clarissa.Woodage@ons.gov.uk>
2025-01-17 12:57:09 +0000

    Testing my data with the carry_forwards function

--- tests/test_imputation/test_CF.py
+++ tests/test_imputation/test_CF.py
@@ -1,105 +1,54 @@
 
-from pandas import DataFrame as pandasDF
+import os
+import pandas as pd
+import pytest
 from pandas._testing import assert_frame_equal
-import numpy as np
+
 from src.imputation.MoR import carry_forwards
+from src.imputation.imputation_helpers import get_imputation_cols
 
 
-class test_carry_forward:
-    """Unit test to check the carry forward imputation method"""
+class test_carry_forward(object):
+    """Tests for carry_forwards."""
 
-    def create_backdata(self):
-        """Create an input_dataframe for the test."""
-        input__columns = [
-            "reference",
-            "instance",
-            "period_year",
-            "200",
-            "201",
-            "211",
-            "601",
-            "604",
-            "formtype",
-            "status",
-            "imp_marker",
-            "imp_class",
-        ]
+    @pytest.fixture(scope="function")
+    def dummy_CF_backdata(self) -> pd.DataFrame:
+        """Dummy backdata used for testing MoR imputation."""
+        fpath = os.path.join("tests/data/imputation/CF_backdata.csv")
+        df = pd.read_csv(fpath)
+        df = df.astype({"reference": "Int64", "instance": "Int64"})
+        return df
 
-        data = [
-            [1031, 0, 2021, None, "AA", np.nan, "CF14 7UB", "Yes", "0006", "Form sent out", "R", "nan_AA"],
-            [1031, 1, 2021, "C", "AA", 100000.0, np.nan, "Yes", "0006", "Check needed", "R", "C_AA"],
-            [1031, 2, 2021, "D", "AA", np.nan, None, "Yes", "0006", "Check needed", "R", "D_AA"],
-            [1032, 0, 2021, np.nan, "L", np.nan, np.nan, np.nan, "0006", "Form sent out", "R", "nan_L"],
-            [1032, 1, 2021, "C", "L", np.nan, np.nan, np.nan, "0006", "Check needed", "R", "C_L"],
-            [1033, 0, 2021, np.nan, "AB", np.nan, np.nan, "No", "0001", "Clear", "R", "nan_AB"],
-            [1033, 1, 2021, "C", "AB", 62500.0, "NY56 9DV", "No", "0001", "Clear", "R", "C_AB"],
-            [1040, 1, 2021, "D", "G", 86000.0, np.nan, np.nan, "0006", "Check needed", "TMI", "D_G"],
-            [1042, 1, 2021, "D", "P", 9000.0, np.nan, "No", "0006", "Check needed", "TMI", "D_P"],
-            [1045, 0, 2021, np.nan, "AH", np.nan, np.nan, "No", "0001", "Form sent out", "R", "nan_AH"],
-            [1045, 1, 2021, "C", "AH", 20000.0, np.nan, "Yes", "0001", "Check needed", "R", "C_AH"],
-            [1045, 2, 2021, "D", "AH", 30000.0, np.nan, "Yes", "0001", "Check needed", "R", "D_AH"],
-            [1046, 0, 2021, np.nan, "AD", None, np.nan, "Yes", "0006", "Form sent out", "R", "nan_AD"],
-            [1046, 1, 2021, "D", "AD", 80500.0, "GJ73 3GB", "Yes", "0006", "Check needed", "R", "D_AD"],
-            [1046, 2, 2021, "C", "AD", 36000.0, "PL6 8BX", "Yes", "0006", "Check needed", "R", "C_AD"],
-            [1046, 3, 2021, np.nan, "AD", np.nan, "RG7 2PQ", "Yes", "0006", "Check needed", "R", "nan_AD"],
-            [1046, 4, 2021, np.nan, "AD", np.nan, "GU16 7HF", "Yes", "0006", "Check needed", "R", "nan_AD"],
-        ]
 
-        backdata_df = pandasDF(data, columns=input__columns)
-        return backdata_df
+    @pytest.fixture(scope="function")
+    def dummy_CF_input(self) -> pd.DataFrame:
+        """A dummy dataframe used for testing MoR imputation."""
+        fpath = os.path.join("tests/data/imputation/CF_input_data.csv")
+        df = pd.read_csv(fpath)
+        df = df.astype({"reference": "Int64", "instance": "Int64"})
+        return df
 
-    def create_input__df(self):
-        """Create an input_ dataframe for the test."""
-        input_columns = [
-            "reference",
-            "instance",
-            "period_year",
-            "200",
-            "201",
-            "211",
-            "601",
-            "602",
-            "604",
-            "formtype",
-            "status",
-            "postcodes_harmonised",
-            "imp_marker",
-            "imp_class",
-        ]
+    @pytest.fixture(scope="function")
+    def expected_CF_output(self) -> pd.DataFrame:
+        """Expected output from carry_forwards."""
+        fpath = os.path.join("tests/data/imputation/expected_CF_output.csv")
+        df = pd.read_csv(fpath)
+        df = df.astype({"reference": "Int64", "instance": "Int64"})
+        return df
 
-        data = [
-            [1031, 0, 2022, None, "AA", np.nan, "CF14 7UB", np.nan, "Yes", "0006", "Form sent out", "CF14 7UB", "no_imputation", "<NA>_AA"],
-            [1031, 0, 2022, None, "AA", 100000.0, np.nan, np.nan, "Yes", "0006", "Form sent out", "CF14 7UB", "no_imputation", "<NA>_AA"],
-            [1031, 0, 2022, None, "AA", np.nan, np.nan, np.nan, "Yes", "0006", "Form sent out", "CF14 7UB", "no_imputation", "<NA>_AA"],
-            [1031, 1, 2022, "C", "AA", np.nan, np.nan, np.nan, np.nan, "0006", "Check needed", "CF14 7UB", "no_imputation", "C_AA"],
-            [1031, 1, 2022, "C", "AA", np.nan, np.nan, np.nan, np.nan, "0006", "Check needed", "CF14 7UB", "no_imputation", "C_AA"],
-            [1031, 1, 2022, "C", "AA", np.nan, np.nan, np.nan, np.nan, "0006", "Check needed", "CF14 7UB", "no_imputation", "C_AA"],
-            [1031, 2, 2022, "D", "AA", np.nan, None, np.nan, np.nan, "0006", "Check needed", "CF14 7UB", "no_imputation", "D_AA"],
-            [1031, 2, 2022, "D", "AA", np.nan, None, np.nan, np.nan, "0006", "Check needed", "CF14 7UB", "no_imputation", "D_AA"],
-            [1031, 2, 2022, "D", "AA", np.nan, None, np.nan, np.nan, "0006", "Check needed", "CF14 7UB", "no_imputation", "D_AA"],
-            [1032, 0, 2022, np.nan, "L", np.nan, np.nan, np.nan, np.nan, "0006", "Form sent out", "CV34 6UX", "no_imputation", "<NA>_L"],
-            [1032, 0, 2022, np.nan, "L", np.nan, np.nan, np.nan, np.nan, "0006", "Form sent out", "CV34 6UX", "no_imputation", "<NA>_L"],
-            [1032, 1, 2022, np.nan, "L", np.nan, np.nan, np.nan, np.nan, "0006", "Check needed", "CV34 6UX", "no_imputation", "<NA>_L"],
-            [1032, 1, 2022, np.nan, "L", np.nan, np.nan, np.nan, np.nan, "0006", "Check needed", "CV34 6UX", "no_imputation", "<NA>_L"],
-            [1033, 0, 2022, np.nan, "AB", np.nan, np.nan, np.nan, "No", "0001", "Clear", "NY56 9DV", "no_imputation", "<NA>_AB"],
-            [1033, 0, 2022, np.nan, "AB", np.nan, np.nan, np.nan, "No", "0001", "Clear", "NY56 9DV", "no_imputation", "<NA>_AB"],
-            [1033, 1, 2022, "C", "AB", 62500.0, "NY56 9DV", np.nan, "No", "0001", "Clear", "NY56 9DV", "no_imputation", "C_AB"],
-            [1033, 1, 2022, "C", "AB", 62500.0, "NY56 9DV", np.nan, "No", "0001", "Clear", "NY56 9DV", "no_imputation", "C_AB"],
-            [1040, 1, 2022, "D", "G", 86000.0, np.nan, np.nan, np.nan, "0006", "Check needed", "UB6 OHE", "no_imputation", "D_AB"],
-            [1042, 1, 2022, "D", "P", 9000.0, np.nan, np.nan, "No", "0006", "Check needed", "AB10 1BL", "no_imputation", "D_AB"],
-            [1045, 0, 2022, np.nan, "AH", np.nan, np.nan, np.nan, "No", "0001", "Form sent out", "WA3 6AE", "no_imputation", "<NA>_AH"],
-            [1045, 0, 2022, np.nan, "AH", np.nan, np.nan, np.nan, "No", "0001", "Form sent out", "WA3 6AE", "no_imputation", "<NA>_AH"],
-            [1045, 0, 2022, np.nan, "AH", np.nan, np.nan, np.nan, "No", "0001", "Form sent out", "WA3 6AE", "no_imputation", "<NA>_AH"],
-            [1045, 1, 2022, "C", "AH", 20000.0, np.nan, np.nan, "Yes", "0001", "Check needed", "WA3 6AE", "no_imputation", "C_AH"],
-            [1045, 1, 2022, "C", "AH", 20000.0, np.nan, np.nan, "Yes", "0001", "Check needed", "WA3 6AE", "no_imputation", "C_AH"],
-            [1045, 1, 2022, "C", "AH", 20000.0, np.nan, np.nan, "Yes", "0001", "Check needed", "WA3 6AE", "no_imputation", "C_AH"],
-            [1045, 2, 2022, "D", "AH", 30000.0, np.nan, np.nan, "Yes", "0001", "Check needed", "WA3 6AE", "no_imputation", "D_AH"],
-            [1045, 2, 2022, "D", "AH", 30000.0, np.nan, np.nan, "Yes", "0001", "Check needed", "WA3 6AE", "no_imputation", "D_AH"],
-            [1045, 2, 2022, "D", "AH", 30000.0, np.nan, np.nan, "Yes", "0001", "Check needed", "WA3 6AE", "no_imputation", "D_AH"],
-        ]
+    def test_carry_forwards(
+            self,
+            dummy_CF_input,
+            dummy_CF_backdata,
+            expected_CF_output,
+            imputation_config):
 
-        input__df = pandasDF(data, columns=input_columns)
-        return input__df
+        impute_vars = get_imputation_cols(imputation_config)
 
-    def create_output_df(self):
-        """Create an output_dataframe for the test."""
+        result_df = carry_forwards(
+            df=dummy_CF_input,
+            backdata=dummy_CF_backdata,
+            impute_vars=impute_vars,
+            config=imputation_config
+        )
+        assert_frame_equal(result_df, expected_CF_output, check_dtype=False, check_exact=False)
