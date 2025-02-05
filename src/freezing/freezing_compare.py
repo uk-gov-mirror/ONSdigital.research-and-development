a72ec4249c48ef36b1e1d921e3beb2a2858cd947
Ryan Lewis <40635968+Ryan2Y79@users.noreply.github.com>
2025-02-05 15:34:29 +0000

    Update freezing_compare.py

--- src/freezing/freezing_compare.py
+++ src/freezing/freezing_compare.py
@@ -57,7 +57,9 @@ def get_amendments(
     if amendments_df.shape[0] > 0:
         for each in numeric_cols:
             amendments_df[f"{each}_diff"] = (
-                abs(amendments_df[f"{each}_updated"] - amendments_df[f"{each}_original"])
+                abs(
+                    amendments_df[f"{each}_updated"] - amendments_df[f"{each}_original"]
+                )
             )
             amendments_df.loc[
                 amendments_df[f"{each}_diff"] > 0.00001,
