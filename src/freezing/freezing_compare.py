7024acd33cc2289f61d78af1c17d8e8962d883f0
Lewis <ryan.lewis@ons.gov.uk>
2025-02-06 16:39:01 +0000

    version 1.0 of the refactored freezing compare

--- src/freezing/freezing_compare.py
+++ src/freezing/freezing_compare.py
@@ -29,20 +29,72 @@ def get_amendments(
     )
     key_cols = ["reference", "period", "instance"]
     numeric_cols = [
-        "202", "203", "204", "205", "206", "207", "209", "210",
-        "211", "212", "214", "216", "218", "219", "220", "221", "222",
-        "223", "225", "226", "227", "228", "229", "237", "242", "243",
-        "244", "245", "246", "247", "248", "249", "250", "405", "406",
-        "407", "408", "409", "410", "411", "412", "501", "502", "503",
-        "504", "505", "506", "507", "508", "602", "701", "702", "703",
-        "704", "705", "706", "707", "709", "711",
+        "202",
+        "203",
+        "204",
+        "205",
+        "206",
+        "207",
+        "209",
+        "210",
+        "211",
+        "212",
+        "214",
+        "216",
+        "218",
+        "219",
+        "220",
+        "221",
+        "222",
+        "223",
+        "225",
+        "226",
+        "227",
+        "228",
+        "229",
+        "237",
+        "242",
+        "243",
+        "244",
+        "245",
+        "246",
+        "247",
+        "248",
+        "249",
+        "250",
+        "405",
+        "406",
+        "407",
+        "408",
+        "409",
+        "410",
+        "411",
+        "412",
+        "501",
+        "502",
+        "503",
+        "504",
+        "505",
+        "506",
+        "507",
+        "508",
+        "602",
+        "701",
+        "702",
+        "703",
+        "704",
+        "705",
+        "706",
+        "707",
+        "709",
+        "711",
     ]
 
     non_numeric_cols = ["200", "201", "601"]
     # numeric_cols_new = [f"{i}_updated" for i in numeric_cols]
-    numeric_cols_diff = [f"{i}_diff" for i in numeric_cols]
+    # numeric_cols_diff = [f"{i}_diff" for i in numeric_cols]
     # non_numeric_cols_new = [f"{i}_updated" for i in non_numeric_cols]
-    non_numeric_cols_diff = [f"{i}_diff" for i in non_numeric_cols]
+    # non_numeric_cols_diff = [f"{i}_diff" for i in non_numeric_cols]
 
     # Inner join on keys to select only records present in both snapshots
     amendments_df = pd.merge(
@@ -53,51 +105,62 @@ def get_amendments(
         suffixes=("_original", "_updated"),
     )
 
-    # If there are any records to amend, calculate differences
     if amendments_df.shape[0] > 0:
-        for each in numeric_cols:
-            amendments_df[f"{each}_diff"] = (
-                amendments_df[f"{each}_updated"] - amendments_df[f"{each}_original"]
-            )
-            amendments_df.loc[
-                amendments_df[f"{each}_diff"] > 0.00001,
-                f"is_{each}_diff_nonzero_or_true",
-            ] = True
 
-        for each in non_numeric_cols:
-            amendments_df[f"is_{each}_diff_nonzero_or_true"] = (
-                amendments_df[f"{each}_updated"] != amendments_df[f"{each}_original"]
-            )
-            amendments_df.loc[
-                amendments_df[f"is_{each}_diff_nonzero_or_true"], f"{each}_diff"
-            ] = amendments_df[f"{each}_updated"]
+        metadata_cols = [
+            col
+            for col in updated_snapshot.columns
+            if col not in numeric_cols + non_numeric_cols
+        ]
 
-        # Take a slice of the df which is just the cols ending with
-        # _diff_nonzero_or_true.
-        # Do a column-wise any() on this slice, which returns a series where the
-        # value is True if any of the *_diff_nonzero_or_true cols in that row were True
-        # Add that series as a column to the original df
-        # Remove any rows from the df where is_any_diff_nonzero_or_true is False
-        amendments_df["is_any_diff_nonzero_or_true"] = amendments_df[
-            amendments_df.columns[
-                amendments_df.columns.str.endswith("_diff_nonzero_or_true")
+        def column_processing(df, type):
+            """Function to split original & updated data and sort to make uniform."""
+            select_cols = [
+                "reference",
+                "period",
+                "instance",
+                *[col for col in df.columns if col.endswith(type)],
             ]
-        ].any(axis="columns")
-        amendments_df = amendments_df.loc[amendments_df["is_any_diff_nonzero_or_true"]]
+            df = df[select_cols]
 
-        # Select the row from the updated snapshot, and differences in key variables
-        select_cols = [
-            "reference",
-            "period",
-            "instance",
-            *[col for col in amendments_df.columns if col.endswith("_updated")],
-            *numeric_cols_diff,
-            *non_numeric_cols_diff,
+            df.columns = df.columns.str.replace("_original", "").str.replace(
+                "_updated", ""
+            )
+
+            df = df.sort_values(by=["reference", "instance", "period"])
+
+            return df
+
+        frozen_df = column_processing(amendments_df, "original")
+        updated_snapshot_df = column_processing(amendments_df, "updated")
+
+        # Create a True False boolian mask for differences between frozen and SPP data.
+        # Numeric differences
+        numeric_differences_mask = (
+            frozen_df[numeric_cols] != updated_snapshot_df[numeric_cols]
+        )
+        numeric_differences_df = (
+            frozen_df[numeric_cols] - updated_snapshot_df[numeric_cols]
+        )
+        numeric_differences_masked_df = numeric_differences_df[numeric_differences_mask]
+
+        # Non-numeric differences
+        non_numeric_differences_mask = (
+            frozen_df[non_numeric_cols] != updated_snapshot_df[non_numeric_cols]
+        )
+        non_numeric_differences_masked_df = updated_snapshot_df[non_numeric_cols][
+            non_numeric_differences_mask
         ]
-        amendments_df = amendments_df[select_cols]
-        amendments_df.columns = [
-            col.replace("_updated", "") for col in amendments_df.columns
-        ]
+
+        # Concatenate the froxen metadata, numeric and non-numeric differences
+        amendments_df = pd.concat(
+            [
+                updated_snapshot[metadata_cols],
+                numeric_differences_masked_df,
+                non_numeric_differences_masked_df,
+            ],
+            axis=1,
+        )
 
         # Add markers
         amendments_df["accept_changes"] = False
