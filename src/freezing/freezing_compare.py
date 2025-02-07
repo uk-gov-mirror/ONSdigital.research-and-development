96c7c6a98c80f94f8eef9b13233143cfdc571926
Lewis <ryan.lewis@ons.gov.uk>
2025-02-07 14:47:53 +0000

    added abs diff calc

--- src/freezing/freezing_compare.py
+++ src/freezing/freezing_compare.py
@@ -91,6 +91,166 @@ def get_amendments(
     ]
 
     non_numeric_cols = ["200", "201", "601"]
+    # numeric_cols_new = [f"{i}_updated" for i in numeric_cols]
+    numeric_cols_diff = [f"{i}_diff" for i in numeric_cols]
+    # non_numeric_cols_new = [f"{i}_updated" for i in non_numeric_cols]
+    non_numeric_cols_diff = [f"{i}_diff" for i in non_numeric_cols]
+
+    # Inner join on keys to select only records present in both snapshots
+    amendments_df = pd.merge(
+        frozen_csv,
+        updated_snapshot,
+        on=key_cols,
+        how="inner",
+        suffixes=("_original", "_updated"),
+    )
+
+    # If there are any records to amend, calculate differences
+    if amendments_df.shape[0] > 0:
+        for each in numeric_cols:
+            amendments_df[f"{each}_diff"] = (
+                amendments_df[f"{each}_updated"] - amendments_df[f"{each}_original"]
+            )
+            amendments_df[f"{each}_abs_diff"] = (
+                amendments_df[f"{each}_updated"] - amendments_df[f"{each}_original"]
+            ).abs()
+            amendments_df.loc[
+                amendments_df[f"{each}_abs_diff"] > 0.00001,
+                f"is_{each}_abs_diff_nonzero_or_true",
+            ] = True
+
+        for each in non_numeric_cols:
+            amendments_df[f"is_{each}_diff_nonzero_or_true"] = (
+                amendments_df[f"{each}_updated"] != amendments_df[f"{each}_original"]
+            )
+            amendments_df.loc[
+                amendments_df[f"is_{each}_diff_nonzero_or_true"], f"{each}_diff"
+            ] = amendments_df[f"{each}_updated"]
+
+        # Take a slice of the df which is just the cols ending with
+        # _diff_nonzero_or_true.
+        # Do a column-wise any() on this slice, which returns a series where the
+        # value is True if any of the *_diff_nonzero_or_true cols in that row were True
+        # Add that series as a column to the original df
+        # Remove any rows from the df where is_any_diff_nonzero_or_true is False
+        amendments_df["is_any_abs_diff_nonzero_or_true"] = amendments_df[
+            amendments_df.columns[
+                amendments_df.columns.str.endswith("_abs_diff_nonzero_or_true")
+            ]
+        ].any(axis="columns")
+        amendments_df = amendments_df.loc[
+            amendments_df["is_any_abs_diff_nonzero_or_true"]
+        ]
+
+        # Select the row from the updated snapshot, and differences in key variables
+        select_cols = [
+            "reference",
+            "period",
+            "instance",
+            *[col for col in amendments_df.columns if col.endswith("_updated")],
+            *numeric_cols_diff,
+            *non_numeric_cols_diff,
+        ]
+        amendments_df = amendments_df[select_cols]
+        amendments_df.columns = [
+            col.replace("_updated", "") for col in amendments_df.columns
+        ]
+
+        # Add markers
+        amendments_df["accept_changes"] = False
+
+        return amendments_df
+    else:
+        FreezingLogger.info("No amendments found.")
+        return None
+
+
+def get_aaaaamendments(
+    frozen_csv: pd.DataFrame,
+    updated_snapshot: pd.DataFrame,
+    FreezingLogger: logging.Logger,
+) -> pd.DataFrame:
+    """Get amended records from updated snapshot.
+
+    Get all records that are present in both the frozen_csv and the updated
+    snapshot, and have matching keys.
+
+    Args:
+        frozen_csv (pd.DataFrame): The staged and validated frozen data.
+        updated_snapshot (pd.DataFrame): The staged and validated updated
+            snapshot data.
+        FreezingLogger (logging.Logger): The logger to log to.
+
+    Returns:
+        amendments_df (pd.DataFrame): The records that have changed.
+    """
+    FreezingLogger.info(
+        "Looking for records that have changed in the updated snapshot."
+    )
+    key_cols = ["reference", "period", "instance"]
+    numeric_cols = [
+        "202",
+        "203",
+        "204",
+        "205",
+        "206",
+        "207",
+        "209",
+        "210",
+        "211",
+        "212",
+        "214",
+        "216",
+        "218",
+        "219",
+        "220",
+        "221",
+        "222",
+        "223",
+        "225",
+        "226",
+        "227",
+        "228",
+        "229",
+        "237",
+        "242",
+        "243",
+        "244",
+        "245",
+        "246",
+        "247",
+        "248",
+        "249",
+        "250",
+        "405",
+        "406",
+        "407",
+        "408",
+        "409",
+        "410",
+        "411",
+        "412",
+        "501",
+        "502",
+        "503",
+        "504",
+        "505",
+        "506",
+        "507",
+        "508",
+        "602",
+        "701",
+        "702",
+        "703",
+        "704",
+        "705",
+        "706",
+        "707",
+        "709",
+        "711",
+    ]
+
+    non_numeric_cols = ["200", "201", "601"]
 
     numeric_cols_diff = [f"{i}_diff" for i in numeric_cols]
     non_numeric_cols_diff = [f"{i}_diff" for i in non_numeric_cols]
