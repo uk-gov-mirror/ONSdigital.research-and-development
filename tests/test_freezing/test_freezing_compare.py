0e2a3cc97b02a53eb3f79d39f447698f0fa9e1af
Lewis <ryan.lewis@ons.gov.uk>
2025-02-07 11:23:41 +0000

    fixed bug in unit test that was causing it to pass, when it should not have been passing

--- tests/test_freezing/test_freezing_compare.py
+++ tests/test_freezing/test_freezing_compare.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import logging
+import numpy as np
 
 from src.freezing.freezing_compare import get_amendments, get_additions
 from src.freezing.freezing_compare import bring_together_split_cases
@@ -16,7 +17,7 @@ class TestGetAmendments:
     def add_numeric_cols(
         self,
         df,
-        expected = False
+        expected=False
     ) -> pd.DataFrame:
         """Numerical columns for test dataframes."""
         numeric_cols = [
@@ -29,17 +30,20 @@ class TestGetAmendments:
             "704", "705", "706", "707", "709", "711",
         ]
         for col in numeric_cols:
-            df[col] = None
+            df[col] = 1.0
         if expected:
             for col in numeric_cols:
-                df[f"{col}_diff"] = None
+                if col != "201":
+                    df[f"{col}_diff"] = 0.0
+                elif col == "201":
+                    df[f"{col}_diff"] = np.nan
         return df
 
 
     # Create test frozen df
     def create_test_frozen_df(self) -> pd.DataFrame:
         """Create a test frozen df."""
-        input_cols = ["reference", "period", "instance", "203", "202", "200", "601"]
+        input_cols = ["reference", "period", "instance", "202", "203", "200", "601"]
         data = [
             ["A", 202412, 2.0, 1.0, 2.0, "A", None],
             ["B", 202412, None, None, 1.0, "B", "C"],
@@ -56,7 +60,7 @@ class TestGetAmendments:
     # Create test amendments df
     def create_test_amendments_df(self) -> pd.DataFrame:
         """Create a test amendments df."""
-        input_cols = ["reference", "period", "instance", "203", "202", "200", "601"]
+        input_cols = ["reference", "period", "instance", "202", "203", "200", "601"]
         data = [
             ["A", 202412, 2.0, 1.0, 2.0, "A", None], # No diffs
             ["B", 202412, None, None, 1.0, "A", "B"], # 200 diff "A"
@@ -73,17 +77,16 @@ class TestGetAmendments:
     # Create expected outcome df
     def create_test_expected_outcome_df(self) -> pd.DataFrame:
         """Create a test expected_outcome df."""
-        input_cols = ["reference", "period", "instance", "203", "202", "200", "601", "203_diff", "202_diff", "200_diff", "601_diff", "accept_changes"]
+        input_cols = ["reference", "period", "instance", "202", "203", "200", "601", "202_diff", "203_diff", "200_diff", "601_diff", "accept_changes"]
         data = [
             ["A", 202412, 2.0, 1.0, 2.0, "A", None, 0.0, 0.0, None, None, False],
             ["B", 202412, None, None, 1.0, "A", "B", None, 0.0, "A", "B", False],
             ["C", 202412, 0.0, 2.0, 2.0, "A", "B", 1.0, 0.0, None, None, False],
             ["D", 202412, 1.0, 2.0, 3.0, "E", "D", 0.0, 0.0, "E", None, False],
             ["E", 202412, None, 10.0, 1.0, "E", "F", 6.0, -4.0, None, None, False],
-            ["R", 202412, 1.0, 4.0, 4.0, "C", "D", -4.0, -4.0, None, None, False]
         ]
         input_expected_outcome_df = pd.DataFrame(data=data, columns=input_cols)
-        input_expected_outcome_df = self.add_numeric_cols(input_expected_outcome_df, expected = True)
+        input_expected_outcome_df = self.add_numeric_cols(input_expected_outcome_df, expected=True)
         return input_expected_outcome_df
 
 
