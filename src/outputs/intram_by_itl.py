ccfcb9c809b76583012863d62d40ad3eb6a887fd
woodac <Clarissa.Woodage@ons.gov.uk>
2025-02-26 13:53:58 +0000

    removing capital letters

--- src/outputs/intram_by_itl.py
+++ src/outputs/intram_by_itl.py
@@ -94,36 +94,36 @@ def aggregate_itl(
     Returns:
         Tuple[pd.DataFrame, pd.DataFrame]: The ITL1 and ITL2 dataframes.
     """
-    CURRENT_YEAR = config["survey"]["survey_year"]
-    GEO_COLS = config["mappers"]["geo_cols"]
-    HC_COLS = (
+    current_year = config["survey"]["survey_year"]
+    geo_cols = config["mappers"]["geo_cols"]
+    hc_cols = (
         config["breakdowns"]["headcount_total"]
         + config["breakdowns"]["emp_total"]
         + config["imputation"]["sum_cols"]
     )
-    BASE_COLS = ["postcodes_harmonised", "formtype", "211"]
-    df = gb_df[GEO_COLS + BASE_COLS + HC_COLS]
+    base_cols = ["postcodes_harmonised", "formtype", "211"]
+    df = gb_df[geo_cols + base_cols + hc_cols]
 
     # conditionally include NI responses to produce UK
     if uk_output:
-        ni_df = ni_df.copy()[["formtype", "211"] + HC_COLS]
-        for col in GEO_COLS + ["postcodes_harmonised"]:
+        ni_df = ni_df.copy()[["formtype", "211"] + hc_cols]
+        for col in geo_cols + ["postcodes_harmonised"]:
             ni_df[col] = pd.NA
         df = df.append(ni_df, ignore_index=True).copy()
 
     # Create the aggregation dictionary
     agg_dict = {"211": "sum"}
-    agg_dict.update({col: "sum" for col in HC_COLS})
+    agg_dict.update({col: "sum" for col in hc_cols})
 
     # Aggregate to ITL2 and ITL1 (Keep 3 and 4 letter codes)
-    itl2 = df.groupby(GEO_COLS).agg(agg_dict).reset_index()
-    itl1 = itl2.drop(GEO_COLS[:2], axis=1).copy()
-    itl1 = itl1.groupby(GEO_COLS[2:]).agg(agg_dict).copy().reset_index()
+    itl2 = df.groupby(geo_cols).agg(agg_dict).reset_index()
+    itl1 = itl2.drop(geo_cols[:2], axis=1).copy()
+    itl1 = itl1.groupby(geo_cols[2:]).agg(agg_dict).copy().reset_index()
 
     # # Clean data rady for export
-    itl2 = itl2.drop(GEO_COLS[2:], axis=1)
-    itl1 = rename_itl(itl1, 1, CURRENT_YEAR)
-    itl2 = rename_itl(itl2, 2, CURRENT_YEAR)
+    itl2 = itl2.drop(geo_cols[2:], axis=1)
+    itl1 = rename_itl(itl1, 1, current_year)
+    itl2 = rename_itl(itl2, 2, current_year)
 
     return itl1, itl2
 
