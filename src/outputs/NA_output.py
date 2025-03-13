0c37f2496e1e37152783a486a140f1fa8014e18d
woodac <Clarissa.Woodage@ons.gov.uk>
2025-03-13 16:45:23 +0000

    working progress

--- src/outputs/NA_output.py
+++ src/outputs/NA_output.py
@@ -40,6 +40,7 @@ def output_na(df: pd.DataFrame, config: dict, write_csv: callable):
     df[cols] = div_df[cols]
 
     # Add columns for back-compatibility
+    # TO DO: Check whether the cols_to_add are civil or defence q's
     df = cols_to_add(df)
 
     # Filter civil and defence data into seperate dataframes
@@ -52,6 +53,8 @@ def output_na(df: pd.DataFrame, config: dict, write_csv: callable):
 
     # Concatenate the dataframes
     df = concat_df(civil_df, defence_df)
+
+    # Columns 5xxx "_C" and "D", need to be combined as one total
 
     # Create output dataframe with required columns from schema
     schema_path = config["schema_paths"]["nation_accounts_schema"]
@@ -81,116 +84,11 @@ def divide_by_1000(df, config):
 
 def cols_to_add(df: pd.DataFrame):
     """Adds columns for back-compatibility."""
-    cols_to_add = ["q0303", "q0327", "q0319", "q0323", "q0208"]
+    cols_to_add = ["q0303", "q0319", "q0320", "q0323", "q0324" "q0208"]
 
     for col in cols_to_add:
         if col not in df.columns:
             df[col] = 0
-
-    return df
-
-
-def map_to_prev_civil(df: pd.DataFrame):
-    """Maps the columns to the orginal questions for civil data."""
-    columns_to_revert = {
-        "210": "q0202",
-        "219": "q0204",
-        "220": "q0206",
-        "221": "q0210",
-        "204": "q0212",
-        "202": "q0214",
-        "222": "q0216",
-        "223": "q0218",
-        "211": "q0222",
-        "205": "q0224",
-        "206": "q0226",
-        "208": "q0228",
-        "212": "q0301",
-        "216": "q0305",
-        "242": "q0307",
-        "243": "q0309",
-        "244": "q0311",
-        "245": "q0313",
-        "246": "q0315",
-        "247": "q0317",
-        "248": "q0321",
-        "218": "q0325",
-        "214": "q0327",
-        "250": "q0329",
-        "405": "q0509",  # FTE Researchers - civil
-        "407": "q0511",  # FTE Technicians - civil
-        "409": "q0513",  # FTE Other - civil
-        "411": "q0515",  # FTE Total - civil
-    }
-
-    for col in columns_to_revert:
-        if col in df.columns:
-            df[columns_to_revert[col]] = df[col]
-            df.drop(columns=col, inplace=True)
-
-    return df
-
-
-def map_to_prev_defence(df: pd.DataFrame):
-    """Maps columns to orginal questions for defence data."""
-    columns_to_revert = {
-        "210": "q0203",
-        "219": "q0205",
-        "220": "q0207",
-        "221": "q0211",
-        "204": "q0213",
-        "202": "q0215",
-        "222": "q0217",
-        "223": "q0219",
-        "211": "q0223",
-        "205": "q0225",
-        "206": "q0227",
-        "207": "q0229",
-        "212": "q0302",
-        "216": "q0306",
-        "242": "q0308",
-        "243": "q0310",
-        "244": "q0312",
-        "245": "q0314",
-        "246": "q0316",
-        "247": "q0318",
-        "248": "q0322",
-        "218": "q0326",
-        "214": "q0328",
-        "250": "q0330",
-        "405": "q0510",  # FTE Researchers- defence
-        "407": "q0512",  # FTE Technicians- defence
-        "409": "q0514",  # FTE Other - defence
-        "411": "q0516",  # FTE Total - defence
-    }
-
-    for col in columns_to_revert:
-        if col in df.columns:
-            df[columns_to_revert[col]] = df[col]
-            df.drop(columns=col, inplace=True)
-
-    return df
-
-
-def reformat_questions(df: pd.DataFrame):
-    """Columns common to both civil and defence that names need to be reverted"""
-    columns_to_revert = {
-        "headcount_res_m": "q0501",
-        "headcount_res_f": "q0502",
-        "headcount_tec_m": "q0503",
-        "headcount_tec_f": "q0504",
-        "headcount_oth_m": "q0505",
-        "headcount_oth_f": "q0506",
-        "headcount_tot_m": "q0507",
-        "headcount_tot_f": "q0508",
-        "601": "q0601",
-        "602": "q0602",
-    }
-
-    for col in columns_to_revert:
-        if col in df.columns:
-            df[columns_to_revert[col]] = df[col]
-            df.drop(columns=col, inplace=True)
 
     return df
 
