c150dcedfb27694ce9eef4648d6c9d6ee166fb9c
Lewis <ryan.lewis@ons.gov.uk>
2025-03-13 17:47:18 +0000

    removed instance 0 deletion for short to long construction

--- src/construction/all_data_construction.py
+++ src/construction/all_data_construction.py
@@ -9,7 +9,6 @@ from src.construction.construction_helpers import (
     prepare_forms_gb,
     clean_construction_type,
     add_constructed_nonresponders,
-    # remore_short_to_long_0,
     finalise_forms_gb,
     replace_values_in_construction,
 )
@@ -75,6 +74,7 @@ def all_data_construction(  # noqa: C901
     # Drop columns without constructed values
     construction_df = construction_df.dropna(axis="columns", how="all")
     if "statusencoded" in construction_df.columns:
+        construction_df = construction_df.copy()
         construction_df["statusencoded"] = (
             construction_df["statusencoded"].astype(str).str.split(".").str[0]
         )
@@ -113,13 +113,6 @@ def all_data_construction(  # noqa: C901
     updated_snapshot_df, construction_df = replace_values_in_construction(
         updated_snapshot_df, construction_df
     )
-
-    # f "construction_type" in construction_df.columns:
-    #   if "short_to_long" in construction_df["construction_type"].values:
-    #       construction_df.reset_index(inplace=True)
-    #       updated_snapshot_df = remove_short_to_long_0(
-    #           updated_snapshot_df, construction_df
-    #       )
 
     # Run GB specific actions
     if not is_northern_ireland:
