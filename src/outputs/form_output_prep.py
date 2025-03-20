e9cf41b480c55f79552ff19c38bfec948fc4bd18
Anne Griffith <anne.griffith@ons.gov.uk>
2025-03-20 14:45:46 +0000

    new function to filter outputs

--- src/outputs/form_output_prep.py
+++ src/outputs/form_output_prep.py
@@ -4,6 +4,24 @@ from src.estimation.apply_weights import apply_weights
 from src.outputs.outputs_helpers import create_period_year
 
 
+def filter_outputs(df: pd.DataFrame) -> pd.DataFrame:
+    """Filters the outputs based on the conditions.
+
+    Args:
+        df (pd.DataFrame): dataframe with weights computed but not applied
+
+    Returns:
+        tau_outputs_df (pd.DataFrame): dataframe filtered for output
+    """
+    no_rnd_spenders_filter = ~((df["604"] == "No") & (df["211"] > 0))
+    nonzero_only_filter = df["211"] > 0
+
+    conditions = no_rnd_spenders_filter & nonzero_only_filter
+    output_df = df.copy().loc[conditions]
+
+    return output_df
+
+
 def form_output_prep(
     weighted_df: pd.DataFrame,
     ni_full_responses: pd.DataFrame,
@@ -23,17 +41,15 @@ def form_output_prep(
     """
     # Deal with "No" in 604, also eliminating spenders
     flag_no_rand_spenders(weighted_df, "error")
-    no_rnd_spenders_filter = ~((weighted_df["604"] == "No") & (weighted_df["211"] > 0))
-    instance_0_filter = weighted_df["instance"] != 0
 
-    tau_outputs_df = weighted_df.copy().loc[no_rnd_spenders_filter & instance_0_filter]
+    tau_outputs_df = filter_outputs(weighted_df)
     tau_outputs_df = create_period_year(tau_outputs_df)
 
     # Now that the tau outputs have been created, we can apply the weights to the
     # weighted_df to get the estimated values.
     estimated_df = apply_weights(weighted_df, config, for_qa=False)
 
-    outputs_df = estimated_df.copy().loc[no_rnd_spenders_filter]
+    outputs_df = filter_outputs(estimated_df)
     outputs_df = create_period_year(outputs_df)
 
     if ni_full_responses is not None:
