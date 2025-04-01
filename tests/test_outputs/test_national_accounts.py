203b4cecfa982fdf2c25074e1073b1ab61405c3f
woodac <Clarissa.Woodage@ons.gov.uk>
2025-04-01 16:15:35 +0100

    updating test

--- tests/test_outputs/test_national_accounts.py
+++ tests/test_outputs/test_national_accounts.py
@@ -11,8 +11,7 @@ from pandas.testing import assert_frame_equal
 
 # Local Imports
 from src.outputs.PNP_NA_output import (
-    divide_by_1000,
-    remove_C_D,
+    divide_by_1000
 )
 
 class TestDivideBy1000(object):
@@ -53,49 +52,6 @@ class TestDivideBy1000(object):
         expected_output = self.expected_output()
 
         result_df = divide_by_1000(input_data, config)
-
-        assert_frame_equal(
-            result_df.reset_index(drop=True),
-            expected_output.reset_index(drop=True),
-            check_dtype=False
-        )
-
-
-class TestRemoveC_D(object):
-    """Test for remove_C_D from specific columns."""
-
-    def input_data (self):
-        """Create input data."""
-        columns = ["ref_C", "211_C", "211_D", "601_C", "602_D"]
-        data = ([1001, 5000, 0, 60, 100],
-                [1002, 3000, 50, 0, 50],
-                [1003, 1000, 0, 100, 50],
-                [1004, pd.NA, 2000, 100, 100],
-                [1005, 0, 3000, 200, 70],
-                [1006, 0, 1500, 100, 30])
-
-        df = pd.DataFrame(data=data, columns=columns)
-        return df
-
-    def expected_output(self):
-        """Create expected output data."""
-        columns = ["ref", "211_C", "211_D", "601", "602"]
-        data = ([1001, 5000, 0, 60, 100],
-                [1002, 3000, 50, 0, 50],
-                [1003, 1000, 0, 100, 50],
-                [1004, pd.NA, 2000, 100, 100],
-                [1005, 0, 3000, 200, 70],
-                [1006, 0, 1500, 100, 30])
-
-        df = pd.DataFrame(data=data, columns=columns)
-        return df
-
-    def test_remove_C_D(self):
-        """Test for remove_C_D."""
-        input_data = self.input_data()
-        expected_output = self.expected_output()
-
-        result_df = remove_C_D(input_data)
 
         assert_frame_equal(
             result_df.reset_index(drop=True),
