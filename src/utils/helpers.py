3803c34a0de0013ac49ff502a08f0b641bac222b
Anne Griffith <anne.griffith@ons.gov.uk>
2025-10-13 14:35:12 +0100

    remove obselete code

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,7 +1,6 @@
 """Define helper functions to be used throughout the pipeline.."""
 
 import yaml
-import tomli
 import pandas as pd
 
 from typing import Union
@@ -28,47 +27,7 @@ class ConfigSettings:
         return config
 
 
-def user_config_reader(configfile: str = user_config_path) -> dict:
-    """Function to parse the userconfig.toml file
-
-    Args:
-        configfile (str, optional): _description_. Defaults to user_config_path.
-
-    Returns:
-        dict: A nested dictionary where the keys are section titles within the TOML
-        file.
-        If only one variable under the section title in the TOML file is given
-        then it is passed directly as a dictionary value. If more than one
-        variable is defined then they are parsed as a dictionary themselves.
-        An example of what is returned is given below:
-
-        {'title': 'TOML Example config', 'period': {'start_period':
-        datetime.date(1990, 10, 10), 'end_period': datetime.date(2000, 10, 5)}}
-    """
-    with open(configfile, "rb") as file:
-        toml_dict = tomli.load(file)
-
-    return toml_dict
-
-
-def period_select() -> tuple:
-    """Function returning the start and end date under consideration.
-
-
-    Returns:
-        tuple: A tuple containing two datetime.date objects. The first is the
-        start date of the period under consideration, the second is the
-        end date of that period.
-        Example:
-            (datetime.date(1990, 10, 10), datetime.date(2000, 10, 5))
-    """
-
-    period_dict = user_config_reader()["period"]
-
-    return period_dict["start_period"], period_dict["end_period"]
-
-
-def convert_formtype(formtype_value: str) -> str:
+def convert_formtype(formtype_value: str) -> str | None:
     """Convert the formtype to a standardised format.
 
     Args:
