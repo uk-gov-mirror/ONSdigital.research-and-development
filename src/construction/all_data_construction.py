7ae2c7bceb889f372e48bfa0759074c90044a597
Anne Griffith <anne.griffith@ons.gov.uk>
2025-10-30 16:53:57 +0000

    update np.NaN to np.nan

--- src/construction/all_data_construction.py
+++ src/construction/all_data_construction.py
@@ -55,8 +55,8 @@ def all_data_construction(  # noqa: C901
             lambda x: clean_construction_type(x)
         )
         # validate that 'construction_type' is valid
-        valid_types = ["short_to_long", "new", np.NaN, "<NA>", "<na>"]
-        if False in list(construction_df.construction_type.isin(valid_types)):
+        valid_types = ["short_to_long", "new", np.nan, "<NA>", "<na>"]
+        if False in list(construction_df.notna().construction_type.isin(valid_types)):
             raise ValueError(
                 f"Invalid value for construction_type. Expected one of {valid_types}"
             )
